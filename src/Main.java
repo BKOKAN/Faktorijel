ea3646004f8a4af23d5f3e7f4e0c2883c7197f68
Bozok <bozokokan11@hotmail.com>
2026-03-03 20:31:45 +0100

    Faktorijel

--- src/Main.java
+++ src/Main.java
@@ -19,7 +19,7 @@ public class Main {
                    faktorijel = true;
                 }
                 if (broj < 0){
-                    System.out.println("Unijeli ste negativan broj. Faktorijel nije definiran za negativne brojeve. Unesite cijeli pozitivan broj.");
+                   throw new InputMismatchException("Niste unijeli cijeli pozitvni broj.");
                 }
             } catch (InputMismatchException e){
                 System.out.println("Niste unijeli cijeli broj. Greska: " + e.getMessage());
